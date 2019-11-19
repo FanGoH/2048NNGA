9f66aa32d32d60a12ffb13fb14fcef4c8cbaa8c7
Hector Osuna <55816095+FanGoH@users.noreply.github.com>
2019-11-19 10:28:04 -0800

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,18 +1,11 @@
-#import numpy as np
+import random as rd
 
 pot2 = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8182, 16364]
 
 class Block:
-    def __init__(self):
-        self.posicion = [0, 0]
-        self.numero = 0
-
-#class NeuralNetwork():
-
-#    def __init__(self):
-#        np.random.seed(1)
-
-#        self.synaptic_weights = 2 * random.random((3, 1) - 1 )
+    def __init__(self, pos):
+        self.posicion = [pos[0], pos[1]]
+        self.numero = rd.choice([2, 4])
 
 class tablero:
     bloques = []
@@ -20,6 +13,14 @@ class tablero:
     def __init__(self):
         self.casillas = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         self.casillasocupadas = self.casillas
+        self.bloques.append(Block([rd.choice([0, 1, 2, 3]), rd.choice([0, 1, 2, 3])]))
+
+    def actualizar(self):
+        self.casillasocupadas = [[0,0,0,0],[0,0,0,0], [0,0,0,0], [0,0,0,0]] 
+        self.casillas = [[0,0,0,0],[0,0,0,0], [0,0,0,0], [0,0,0,0]] 
+        for bloque in self.bloques:
+            self.casillas[bloque.posicion[1]][bloque.posicion[0]] = bloque.numero
+            self.casillasocupadas[bloque.posicion[1]][bloque.posicion[0]] = 1
 
     def imprimir(self):
         print(self.casillas[0])
@@ -40,14 +41,30 @@ class tablero:
         
         if direccion[1] == -1:
             for bloque in self.bloques:
-                while bloque.posicion[1] - 1 >= 0:
-                    bloque.posicion[1] = bloque.posicion[0] - 1
+                while bloque.posicion[1] + 1 < 4:
+                    bloque.posicion[1] = bloque.posicion[1] + 1
         
-        if direccion[0] == -1:
+        if direccion[1] == 1:
             for bloque in self.bloques:
-                while bloque.posicion[1] + 1 > 4:
-                    bloque.posicion[1] = bloque.posicion[0] - 1
+                while bloque.posicion[1] - 1 >= 0:
+                    bloque.posicion[1] = bloque.posicion[1] - 1
+
+    def ciclo(self):
+        opc = input("W = Up, A = Left, S = Down, D = Right: ")
+        if opc == 'W':
+            self.movement([0,1])
+        elif opc == 'A':
+            self.movement([-1, 0])
+        elif opc == 'S':
+            self.movement([0,-1])
+        elif opc == 'D':
+            self.movement([1, 0])
+        self.actualizar()
+        
 
 board = tablero()
+board.actualizar()
 
-board.imprimir()
+while True:
+    board.imprimir()
+    board.ciclo()
