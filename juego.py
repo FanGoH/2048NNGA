0f69f6a496f0c5ee85ae5370d162a4988631e6c3
Hector Osuna <55816095+FanGoH@users.noreply.github.com>
2019-11-19 13:29:42 -0800

    Agregado el txt de dificultades

--- juego.py
+++ juego.py
@@ -1,13 +1,13 @@
 import random as rd
 
-pot2 = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8182, 16364]
-
 class Block:
     def __init__(self, pos):
         self.posicion = [pos[0], pos[1]]
         self.numero = rd.choice([2, 4])
     
     def __repr__(self):
+        if self.numero == 0:
+            return "-"
         return str(self.numero)
 
 class tablero:
@@ -36,15 +36,91 @@ class tablero:
             for bloque in self.bloques:
                 cantidad = 0
                 indice = bloque.posicion[0] - 1
+                combinar = False
+                while indice >= 0:
+                    if self.casillasocupadas[bloque.posicion[1]][indice] == 1:
+                        if cantidad == 0 and self.casillas[bloque.posicion[1]][indice].numero == bloque.numero:
+                            combinar = True
+                            bloque2 = self.casillas[bloque.posicion[1]][indice]
+                        cantidad = cantidad + 1
+                    indice = indice - 1
+                bloque.posicion[0] = 0 + cantidad
+                if combinar:
+                    self.merge(direccion, bloque.posicion[0], bloque, bloque2)
+        
+        if direccion[0] == 1:
+            for bloque in self.bloques:
+                cantidad = 0
+                indice = bloque.posicion[0] + 1
+                combinar = False
+                while indice < 4:
+                    if self.casillasocupadas[bloque.posicion[1]][indice] == 1:
+                        if cantidad == 0 and self.casillas[bloque.posicion[1]][indice].numero == bloque.numero:
+                            combinar = True
+                            bloque2 = self.casillas[bloque.posicion[1]][indice]
+                        cantidad = cantidad + 1
+                    indice = indice + 1
+                bloque.posicion[0] = 3 - cantidad
+                if combinar:
+                    self.merge(direccion, bloque.posicion[0], bloque, bloque2)
+
+        if direccion[1] == -1:
+            for bloque in self.bloques:
+                cantidad = 0
+                indice = bloque.posicion[1] + 1
+                combinar = False
+                while indice < 4:
+                    if self.casillasocupadas[indice][bloque.posicion[0]] == 1:
+                        if cantidad == 0 and self.casillas[indice][bloque.posicion[0]].numero == bloque.numero:
+                            combinar = True
+                            bloque2 = self.casillas[indice][bloque.posicion[0]]
+                        cantidad = cantidad + 1
+                    indice = indice + 1
+                bloque.posicion[1] = 3 - cantidad
+                if combinar:
+                    self.merge(direccion, bloque.posicion[1], bloque, bloque2)
+
+        if direccion[1] == 1:
+            for bloque in self.bloques:
+                cantidad = 0
+                indice = bloque.posicion[1] - 1
+                combinar = False
+                while indice >= 0:
+                    if self.casillasocupadas[indice][bloque.posicion[0]] == 1:
+                        if cantidad == 0 and self.casillas[indice][bloque.posicion[0]].numero == bloque.numero:
+                            combinar = True
+                            bloque2 = self.casillas[indice][bloque.posicion[0]]
+                        cantidad = cantidad + 1
+                    indice = indice - 1
+                bloque.posicion[1] = 0 + cantidad
+                if combinar:
+                    self.merge(direccion, bloque.posicion[1], bloque, bloque2)
+
+    def merge(self, direccion, hilera, bloque, bloque2):
+        self.actualizar
+        bloque2.numero = bloque2.numero + bloque.numero
+        eje = [bloque.posicion[1] + direccion[1], bloque.posicion[0] - direccion[0]]
+        temp = []
+        while eje[0] >= 0 and eje[0] < 4 and eje[1] >= 0 and eje[1] < 4:
+            if(self.casillasocupadas[eje[0]][eje[1]] == 1):
+                temp.append(self.casillas[eje[0]][eje[1]])
+            eje[0] = eje[0] + direccion[1]
+            eje[1] = eje[1] - direccion[0]
+
+        self.bloques.remove(bloque)
+        
+        for bloque in temp:
+
+            if direccion[0] == -1:
+                cantidad = 0
+                indice = bloque.posicion[0] - 1
                 while indice >= 0:
                     if self.casillasocupadas[bloque.posicion[1]][indice] == 1:
                         cantidad = cantidad + 1
                     indice = indice - 1
                 bloque.posicion[0] = 0 + cantidad
-
-        
-        if direccion[0] == 1:
-            for bloque in self.bloques:
+            
+            if direccion[0] == 1:
                 cantidad = 0
                 indice = bloque.posicion[0] + 1
                 while indice < 4:
@@ -52,9 +128,8 @@ class tablero:
                         cantidad = cantidad + 1
                     indice = indice + 1
                 bloque.posicion[0] = 3 - cantidad
-
-        if direccion[1] == -1:
-            for bloque in self.bloques:
+            
+            if direccion[1] == -1:
                 cantidad = 0
                 indice = bloque.posicion[1] + 1
                 while indice < 4:
@@ -63,8 +138,7 @@ class tablero:
                     indice = indice + 1
                 bloque.posicion[1] = 3 - cantidad
 
-        if direccion[1] == 1:
-            for bloque in self.bloques:
+            if direccion[1] == 1:
                 cantidad = 0
                 indice = bloque.posicion[1] - 1
                 while indice >= 0:
@@ -73,13 +147,11 @@ class tablero:
                     indice = indice - 1
                 bloque.posicion[1] = 0 + cantidad
 
-    #def merge(self, direccion):
-    #    for bloque in self.bloques:
-    #        if(bloque.)
-
+        self.actualizar()
 
     def ciclo(self):
         opc = input("W = Up, A = Left, S = Down, D = Right: ")
+        opc = opc.capitalize()
         if opc == 'W':
             self.movement([0,1])
         elif opc == 'A':
