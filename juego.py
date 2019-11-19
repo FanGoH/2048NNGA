f31dea496859e3af6d9a3155b74fcc46c4b21ed4
Hector Osuna <55816095+FanGoH@users.noreply.github.com>
2019-11-19 11:22:09 -0800

    Update juego.py

--- juego.py
+++ juego.py
@@ -31,23 +31,56 @@ class tablero:
     def movement(self, direccion):
         if direccion[0] == -1:
             for bloque in self.bloques:
-                while bloque.posicion[0] - 1 >= 0:
-                    bloque.posicion[0] = bloque.posicion[0] - 1
+#                while bloque.posicion[0] - 1 >= 0 and self.casillasocupadas[bloque.posicion[1]][bloque.posicion[0] -1] == 0:
+#                   bloque.posicion[0] = bloque.posicion[0] - 1
+                cantidad = 0
+                indice = bloque.posicion[0] - 1
+                while indice >= 0:
+                    if self.casillasocupadas[bloque.posicion[1]][indice] == 1:
+                        cantidad = cantidad + 1
+                    indice = indice - 1
+                bloque.posicion[0] = 0 + cantidad
+
         
         if direccion[0] == 1:
             for bloque in self.bloques:
-                while bloque.posicion[0] + 1 < 4:
-                    bloque.posicion[0] = bloque.posicion[0] + 1
-        
+#                while bloque.posicion[0] + 1 < 4 and self.casillasocupadas[bloque.posicion[1]][bloque.posicion[0]+1] == 0:
+#                   bloque.posicion[0] = bloque.posicion[0] + 1
+                cantidad = 0
+                indice = bloque.posicion[0] + 1
+                while indice < 4:
+                    if self.casillasocupadas[bloque.posicion[1]][indice] == 1:
+                        cantidad = cantidad + 1
+                    indice = indice + 1
+                bloque.posicion[0] = 3 - cantidad
+
         if direccion[1] == -1:
             for bloque in self.bloques:
-                while bloque.posicion[1] + 1 < 4:
-                    bloque.posicion[1] = bloque.posicion[1] + 1
-        
+#                while bloque.posicion[1] + 1 < 4 and self.casillasocupadas[bloque.posicion[1]+1][bloque.posicion[0]] == 0: 
+#                    bloque.posicion[1] = bloque.posicion[1] + 1
+                cantidad = 0
+                indice = bloque.posicion[1] + 1
+                while indice < 4:
+                    if self.casillasocupadas[indice][bloque.posicion[0]] == 1:
+                        cantidad = cantidad + 1
+                    indice = indice + 1
+                bloque.posicion[1] = 3 - cantidad
+
         if direccion[1] == 1:
             for bloque in self.bloques:
-                while bloque.posicion[1] - 1 >= 0:
-                    bloque.posicion[1] = bloque.posicion[1] - 1
+#                while bloque.posicion[1] - 1 >= 0 and self.casillasocupadas[bloque.posicion[1]-1][bloque.posicion[0]] == 0:
+#                    bloque.posicion[1] = bloque.posicion[1] - 1
+                cantidad = 0
+                indice = bloque.posicion[1] - 1
+                while indice >= 0:
+                    if self.casillasocupadas[indice][bloque.posicion[0]] == 1:
+                        cantidad = cantidad + 1
+                    indice = indice - 1
+                bloque.posicion[1] = 0 + cantidad
+
+    def merge(self, direccion):
+        for bloque in bloques
+
 
     def ciclo(self):
         opc = input("W = Up, A = Left, S = Down, D = Right: ")
@@ -59,9 +92,14 @@ class tablero:
             self.movement([0,-1])
         elif opc == 'D':
             self.movement([1, 0])
+        valido = False
+        while not valido:
+            nuevo = Block([rd.choice([0,1,2,3]), rd.choice([0,1,2,3])])
+            if self.casillasocupadas[nuevo.posicion[1]][nuevo.posicion[0]] == 0:
+                self.bloques.append(nuevo)
+                valido = True
         self.actualizar()
         
-
 board = tablero()
 board.actualizar()
 
